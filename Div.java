d3c8df3fbd15c46d2f2e733561e0a2dd11e27eb8
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-09 17:43:35 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,12 @@
+class Div
+{
+public static void main(String []args)
+{
+System.out.println("The two number is :");
+int a=22;
+int b=11;
+System.out.println(a+"  "+b);
+int Div=a/b;
+System.out.println("the result is:"+Div);
+}
+}
