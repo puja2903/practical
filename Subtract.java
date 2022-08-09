da59b16490970d4119be00f91ef06023128d6b9a
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-09 17:29:50 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,13 @@
+class Subtract
+{
+public static void main (String []args)
+{
+System.out.println("The two number is: ");
+
+int a=17;
+int b=6;
+ System.out.println(a+"  "+b);
+int subtract=a-b;
+System.out.println("the subtract is:"+ subtract);
+}
+}
