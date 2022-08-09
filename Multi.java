260fee35fb90efd7584277e02a31b5fd7b81224d
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-09 17:36:23 +0530

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,12 @@
+ class Multi
+{
+public static void main(String []args)
+{
+System.out.println("The two number is:");
+int a=5;
+int b=6;
+System.out.println(a+"   "+b);
+int multi=a*b;
+System.out.println("The multi is:"+multi);
+}
+}
