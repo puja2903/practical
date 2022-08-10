a2a4b4eb239806d880d58618b1fb89ba34bdc8cf
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-10 16:43:47 +0530

    Add files via upload

---
+++ Rec.java
@@ -0,0 +1,15 @@
+class Rec
+{
+public static void main(String []args)
+{
+int length=4;
+int breadth=2;
+
+System.out.println("enter value of length: "+length);
+System.out.println("enter the value of bredth: "+breadth);
+int area=length*breadth;
+int perimeter =2*(length+breadth);
+System.out.println("area of rectangle is:"+area);
+System.out.println("perimeter of rectangle is:"+perimeter);
+}
+}
