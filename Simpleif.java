b40cb7ed3dacbf970d633c2c96106cd136ea4212
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-20 08:45:18 +0530

    Add files via upload

---
+++ Simpleif.java
@@ -0,0 +1,16 @@
+
+public class Simpleif {
+
+	public static void main(String[] args) {
+		int x=8;
+		int y=4;
+		if (x+y <15)
+		{
+			System.out.println("x+y  is less than 15");
+			
+		}
+		
+
+	}
+
+}
