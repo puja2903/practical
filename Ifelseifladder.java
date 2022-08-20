05a4a5817abac6eba8905e3b64b8e79d66872f78
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-20 10:30:48 +0530

    Add files via upload

---
+++ Ifelseifladder.java
@@ -0,0 +1,24 @@
+package Operator;
+
+public class Ifelseifladder {
+
+	public static void main(String[] args) {
+		int x=34;
+		if(x==10)
+		{
+			System.out.println("value of x is 10");
+		}
+		else if(x==20)
+		{
+			System.out.println("value of x is 20");
+		}
+		else if(x==30)
+		{
+			System.out.println("value of x is 30");
+		}
+		else
+		{
+			System.out.println("this is else statement");
+		}
+	}
+}
