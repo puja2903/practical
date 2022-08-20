e89c3b0339bc4ada5d14646fd792487258fc4e75
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-20 10:34:11 +0530

    Add files via upload

---
+++ Nestedif.java
@@ -0,0 +1,25 @@
+
+public class Nestedif {
+
+	public static void main(String[] args) {
+		String address="Kolkata,India";
+		if (address.endsWith("India"))
+		{
+			if (address.contains("Dunlop"))
+			{
+				System.out.println("your city is Dunlop");
+			}
+			else if(address.contains("Newtown"))
+			{
+				System.out.println("your city is Newtown");	
+			}
+			else 
+			{
+				System.out.println(address.split(",")[1]);
+			}	
+		}
+	}
+}
+	
+
+
