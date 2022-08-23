422cdb81468b1fb5996714e8a7a4a6cc685d24a1
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-23 20:45:23 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=9;i++){
+		
+			for(int j=1;j<=i;j++){
+			System.out.print(j);
+	}
+System.out.println();
+
+}
+	}}
