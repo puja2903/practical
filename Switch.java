a3799b570d9adb18217edfe72a6410290a9f5cf6
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-23 20:41:33 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,25 @@
+package Operator;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int num=10;//declaring a variable for switch
+		//switch expression
+		switch(num) {
+		//case statement
+		case 1:System.out.println("12");
+		break;
+		case 12:System.out.println("15");
+		break;
+		case 10:System.out.println("very good");
+		break;
+		case 15:System.out.println("20");
+		break;
+		//default case statement
+		default:System.out.println("the number is not within the range.");
+		
+		}
+
+	}
+
+}
