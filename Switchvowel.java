7af71cd11bb0ae887f8cf8f78860506ab2401f49
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-23 20:43:48 +0530

    Add files via upload

---
+++ Switchvowel.java
@@ -0,0 +1,21 @@
+package Operator;
+
+public class Switchvowel {
+
+	public static void main(String[] args) {
+		char a='e';
+		switch(a) {
+		case'm':System.out.println(" its not vowel");
+		break;
+		case'e':System.out.println(" its vowel");
+		break;
+		case'y':System.out.println(" its not vowel");
+		break;
+		case'o':System.out.println(" its vowel");
+		break;
+		default:System.out.println("consopnent");
+		}
+
+	}
+
+}
