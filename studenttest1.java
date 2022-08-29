c230f3d763cedcf2b89a79cdbcd9e7d7d97d622e
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-08-29 22:38:02 +0530

    Add files via upload

---
+++ studenttest1.java
@@ -0,0 +1,11 @@
+
+public class studenttest1 {
+	int age;
+	String name;
+	class studenttest{
+	public static void main(String[] args) {
+		studenttest1 ps=new studenttest1();
+		ps.age=25;
+		ps.name="sovan";
+		System.out.println(ps.age+" "+ps.name);
+	}}}
