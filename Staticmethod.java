18f9a37750322c6d122616bf73c9beb59f7cd2bc
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-09-02 14:13:01 +0530

    Add files via upload

---
+++ Staticmethod.java
@@ -0,0 +1,13 @@
+package Method;
+
+public class Staticmethod {
+
+	public static void main(String[] args) {
+display();
+	}
+	private static void display() {
+		System.out.println("I am learning.");
+	}
+	
+
+}
