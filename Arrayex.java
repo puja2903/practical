c4b3d09d840504f879a50d2dd949bb901640d935
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-09-26 17:25:03 +0530

    Add files via upload

---
+++ Arrayex.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Arrayex {
+public static void main(String[] args) {
+/*
+	int a[]=new int[3];//declaration array and object creation 
+   a[0]=10;//initialization
+    a[1]=20;
+    a[2]=30;*/
+	
+	
+	int a[]={10,20,30};  //initialized ,declaration,and object creation
+    //traversing array using for each loop
+   for(int i:a)
+    System.out.println(i);
+   
+   
+    
+ /*traversing array using for loop(10 20 30)
+   for(int i=0;i<a.length;i++)
+   	System.out.println(a[i]);*/
+}
+}
