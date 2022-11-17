da446cc0cf218514822921fc1319dcc05e150dec
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-11-17 11:29:55 +0530

    Add files via upload

---
+++ Lambdaex.java
@@ -0,0 +1,15 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class Lambdaex {
+public static void main(String[] args) {
+	ArrayList<Integer> num= new ArrayList<Integer>();
+num.add(5);
+num.add(4);
+num.forEach((val)->{System.out.println(val);});
+
+	
+	
+}
+}
